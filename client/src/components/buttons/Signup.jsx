f6fd1ca3ca06acfb4c9f3e30066daf3cb0ff9e52
Hadzhi95 <kreyg1992@bk.ru>
2022-03-12 01:20:24 +0300

    Components(Cart, Login, Signup)Fixed

--- client/src/components/buttons/Signup.jsx
+++ client/src/components/buttons/Signup.jsx
@@ -30,15 +30,15 @@ const Signup = () => {
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                                    <input type="email" className="form-control" id="exampleInputEmail11" aria-describedby="emailHelp" />
+                                    <div id="emailHelp1" className="form-text">We'll never share your email with anyone else.</div>
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                                    <input type="password" className="form-control" id="exampleInputPassword11" />
                                 </div>
                                 <div className="mb-3 form-check">
-                                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                    <input type="checkbox" className="form-check-input" id="exampleCheck11" />
                                     <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
                                 </div>
                                 <button type="submit" className="btn btn-outline-primary w-100 mt-5">Register</button>
